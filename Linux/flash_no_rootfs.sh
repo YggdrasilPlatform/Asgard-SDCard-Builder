d8cb84164f49fed327836771244784b7e2300213
WerWolv <werwolv98@gmail.com>
2021-06-04 15:55:43 +0200

    Added scripts to only do partial build/flash work for debugging

---
+++ Linux/flash_no_rootfs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set +x
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 [drive]"
+    echo "  e.g $0 /dev/sdb"
+    exit 1
+fi
+
+DRIVE_PATH=$1
+
+sudo umount ${DRIVE_PATH}[1-9]
+FSBL1=${DRIVE_PATH}1
+FSBL2=${DRIVE_PATH}2
+SSBL=${DRIVE_PATH}3
+ROOTFS=${DRIVE_PATH}4
+
+ROOTFS_MOUNT=/media/asgard_rootfs
+
+sudo mkdir -p ${ROOTFS_MOUNT}
+sudo mount ${ROOTFS} ${ROOTFS_MOUNT}
+
+echo "Flashing u-boot..."
+
+sudo dd if=./asgard_u-boot/spl/u-boot-spl.stm32 of=${FSBL1}
+sudo dd if=./asgard_u-boot/spl/u-boot-spl.stm32 of=${FSBL2}
+sudo dd if=./asgard_u-boot/u-boot.img of=${SSBL}
+
+echo "Copying Kernel image..."
+
+sudo cp -v ./asgard_linux/arch/arm/boot/zImage ${ROOTFS_MOUNT}/boot
+
+echo "Copying Device Tree Blobs..."
+
+sudo mkdir -p ${ROOTFS_MOUNT}/boot/dtbs/kernel
+sudo cp -v ./asgard_linux/arch/arm/boot/dts/stm32mp*.dtb ${ROOTFS_MOUNT}/boot/dtbs/kernel/
+
+echo "Syncing..."
+sync
+
+sudo umount ${ROOTFS_MOUNT}
+
+echo "\nFlashing successul!"
+
+sudo rm -rf ${ROOTFS_MOUNT}
+
+exit 0
